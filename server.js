fa996bd1422a0829d131423c4e1dfab7e8829811
divya-kanaparti <divyakanaparti@gmail.com>
2025-11-26 12:33:58 +0530

    fix mysql env variables

--- server.js
+++ server.js
@@ -12,11 +12,18 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // MYSQL CONNECTION
+// const db = mysql.createConnection({
+//   host: "process.env.localhost",
+//   user: "process.env.root",
+//   password: "process.env.divya",
+//   database: "process.env.test"
+// });
 const db = mysql.createConnection({
-  host: "process.env.localhost",
-  user: "process.env.root",
-  password: "process.env.divya",
-  database: "process.env.test"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 db.connect((err) => {
